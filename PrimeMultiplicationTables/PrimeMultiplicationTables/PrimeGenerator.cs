dc01648ecb51e30098e9c4f9c0936f4d71a57d48
Andrew Pond <a.r.pond@dundee.ac.uk>
2016-10-22 11:29:45 +0100

    Added empty classes for prime generation

---
+++ PrimeMultiplicationTables/PrimeMultiplicationTables/PrimeGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeMultiplicationTables
+{
+    /// <summary>
+    /// Class for generating prime numbers
+    /// </summary>
+    class PrimeGenerator
+    {
+    }
+}
---
+++ PrimeMultiplicationTables/PrimeMultiplicationTables/Test_PrimeGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeMultiplicationTables
+{
+    /// <summary>
+    /// Class for testing the prime generator class
+    /// </summary>
+    class Test_PrimeGenerator
+    {
+    }
+}
