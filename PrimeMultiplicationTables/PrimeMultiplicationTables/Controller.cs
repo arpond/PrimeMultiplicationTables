67cdadb6fa3cd6f286f5eaccfd8e7e1a1925c8fc
Andrew Pond <a.r.pond@dundee.ac.uk>
2016-10-22 15:11:45 +0100

    Checks if empty input string is valid. Additional adjustments to tests.

--- PrimeMultiplicationTables/PrimeMultiplicationTables/Controller.cs
+++ PrimeMultiplicationTables/PrimeMultiplicationTables/Controller.cs
@@ -8,6 +8,22 @@ namespace PrimeMultiplicationTables
 {
     class Controller
     {
-
+        /// <summary>
+        /// Checks if the input is a valid number 
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <returns>Tuple of it's validity and the value itself(otherwise -1)</returns>
+        public static Tuple<bool, int> ValidateInput(String input)
+        {
+            int number;
+            if (Int32.TryParse(input, out number))
+            {
+                return new Tuple<bool, int>(true, number); 
+            }
+            else
+            {
+                return new Tuple<bool, int>(false, -1);
+            }
+        }
     }
 }
--- PrimeMultiplicationTables/PrimeMultiplicationTables/Tests/Tests_Controller.cs
+++ PrimeMultiplicationTables/PrimeMultiplicationTables/Tests/Tests_Controller.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace PrimeMultiplicationTables.Tests
 {
     public class Tests_Controller
     {
-        
+        /// <summary>
+        /// Check that the empty input is invalid
+        /// </summary>
+        [Fact]
+        public void ValidateEmptyInput()
+        {
+            var result = new Tuple<bool, int>(false, -1);
+            Assert.Equal(result,Controller.ValidateInput(""));
+        }
     }
 }
